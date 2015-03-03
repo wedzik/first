f584519b5940da2ccdd19c17827d0ce9a9a3ea8e
wedzik <wedzikmail@gmail.com>
2015-03-03 18:08:12 +0200

    Change db server o posgres

--- app/controllers/admin/super_admins_controller.rb
+++ app/controllers/admin/super_admins_controller.rb
@@ -5,7 +5,7 @@ class Admin::SuperAdminsController < ApplicationController
     @super_admin ||= current_user
     authorize! :update, @super_admin
     respond_to do |format|
-      if @super_admin.update_attributes(params[:super_admin])
+      if @super_admin.update_attributes(super_admin_params)
         flash[:notice] = "Saved"
       end
       format.js
@@ -28,10 +28,14 @@ class Admin::SuperAdminsController < ApplicationController
 
   def update_avatar
     @super_admin ||= Admin.find(current_user.id)
-    if @super_admin.update_attributes(params[:super_admin])
+    if @super_admin.update_attributes(super_admin_params)
       flash[:notice] = "Saved"
     end
     redirect_to admin_profile_path
   end
 
+  private
+  def super_admin_params
+    params.require(:super_admin).permit(:email, :password, :password_confirmation, :first_name, :last_name, :age, :avatar)
+  end
 end
